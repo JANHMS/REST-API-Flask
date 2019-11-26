1b5c28dd00d25cf7db993458da9a040b09c43e82
Janneman <Jan@kooperativ.net>
2019-11-26 12:48:17 +0100

    Mailgun

--- Advanced_RESTful_API_Flask/models/user.py
+++ Advanced_RESTful_API_Flask/models/user.py
@@ -1,4 +1,12 @@
+from requests import Response, post
+from flask import request, url_for
+
 from db import db
+
+MAILGUN_DOMAIN = "sandboxd47a4c5f09a4425eb34409c5ee5eb1f7.mailgun.org"
+MAILGUN_API_KEY = "ecfce605746983935b79333abcc9c505-e470a504-0d5952cc"
+FROM_TITLE = "YOUR EMAIL VERIFICATION"
+FROM_EMAIL = "postmaster@sandboxd47a4c5f09a4425eb34409c5ee5eb1f7.mailgun.org"
 
 
 class UserModel(db.Model):
@@ -7,6 +15,7 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
     activated = db.Column(db.Boolean, default=False)
 
     @classmethod
@@ -14,8 +23,29 @@ class UserModel(db.Model):
         return cls.query.filter_by(username=username).first()
 
     @classmethod
+    def find_by_email(cls, email: str) -> "UserModel":
+        return cls.query.filter_by(email=email).first()
+
+    @classmethod
     def find_by_id(cls, _id: int) -> "UserModel":
         return cls.query.filter_by(id=_id).first()
+
+    def send_confirmation_email(self) -> Response:
+        # string[:-1] means copying from start (inclusive) to the last index (exclusive), a more detailed link below:
+        # from `http://127.0.0.1:5000/` to `http://127.0.0.1:5000`, since the url_for() would also contain a `/`
+        # https://stackoverflow.com/questions/509211/understanding-pythons-slice-notation
+        link = request.url_root[:-1] + url_for("userconfirm", user_id=self.id)
+
+        return post(
+            f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
+            auth=("api", MAILGUN_API_KEY),
+            data={
+                "from": f"{FROM_TITLE} <{FROM_EMAIL}>",
+                "to": self.email,
+                "subject": "Registration confirmation",
+                "text": f"Please click the link to confirm your registration: {link}",
+            },
+        )
 
     def save_to_db(self) -> None:
         db.session.add(self)

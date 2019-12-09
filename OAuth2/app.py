7fa1a18a7c81c3d6d3ff61db8d076fe95d5355b5
Janneman <Jan@kooperativ.net>
2019-12-09 11:09:13 +0100

    Github Login resource

--- OAuth2/app.py
+++ OAuth2/app.py
@@ -4,13 +4,16 @@ from flask_jwt_extended import JWTManager
 from marshmallow import ValidationError
 from dotenv import load_dotenv
 
+load_dotenv(".env")
+
 from db import db
 from ma import ma
-from resources.user import UserRegister, UserLogin, User
 from oa import oauth
+from resources.user import UserRegister, UserLogin, User
+from resources.github_login import GithubLogin
+
 
 app = Flask(__name__)
-load_dotenv(".env")
 app.config.from_object("default_config")
 app.config.from_envvar("APPLICATION_SETTINGS")
 api = Api(app)
@@ -30,9 +33,10 @@ def handle_marshmallow_validation(err):
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/user/<int:user_id>")
 api.add_resource(UserLogin, "/login")
+api.add_resource(GithubLogin, "/login/github")
 
 if __name__ == "__main__":
     db.init_app(app)
     ma.init_app(app)
-    oa.init_app(app)
+    oauth.init_app(app)
     app.run(port=5000)
